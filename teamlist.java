9c0e4347467ac0bd3c46bd20d0d0bec40ebb0c79
fareya <fareya.ikram@gmail.com>
2019-01-13 15:12:55 -0500

    fareya's name

--- teamlist.java
+++ teamlist.java
@@ -2,6 +2,7 @@ public class teamlist {
     public static void main(String[] args) {
         System.out.println("Rasheeda Samih");
         System.out.println("Yossef Naim");
+        System.out.println("Fareya Ikram");
         System.out.println("Grant Ferguson");
         System.out.println("Gabriel Rodriguez");
     }
