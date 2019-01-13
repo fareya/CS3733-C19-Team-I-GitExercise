dbf86b011eca3c0a5b08b5a857d1f61b5f13096e
Carkin <ewcarkin@wpi.edu>
2019-01-13 15:09:10 -0500

    Added my name

--- teamlist.java
+++ teamlist.java
@@ -2,5 +2,6 @@ public class teamlist {
     public static void main(String[] args) {
         System.out.println("Rasheeda Samih");
         System.out.println("Grant Ferguson");
+        System.out.println("Eric Carkin");
     }
 }
